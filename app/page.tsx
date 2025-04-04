0cf1646c47f5ad62c9e84f8d16abbdd66e61b8a9
adamn1225 <anoah1225@gmail.com>
2025-04-04 18:12:39 -0400

    rm

--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,7 @@ export default function SignupPage() {
     <div className="flex justify-center items-center min-h-screen bg-gray-100">
       <div className=" w-full p-6 bg-white rounded shadow">
         <div className="flex justify-center mb-4">
-          <img src={logo.src} alt="Logo" className="h-full w-auto" />
+
         </div>
         <Affilaform />
       </div>
